fc0f8654b69283f85cccb8e0b64b0a5fff4aecb2
James Simpson <10970100+jsimpso@users.noreply.github.com>
2023-12-15 20:52:08 +0800

    Disable provider mirror

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
 set -e
-echo '
-provider_installation {
-  filesystem_mirror {
-    path = "/opt/providers"
-  }
-}' > ~/.terraformrc
+#echo '
+#provider_installation {
+#  filesystem_mirror {
+#    path = "/opt/providers"
+#  }
+#}' > ~/.terraformrc
 
 mkdir -p ~/.local/share/juju
 
