623a67e9565ac9d52ae9980df677cd97714a9272
James Simpson <10970100+jsimpso@users.noreply.github.com>
2023-12-15 16:43:50 +0800

    Add providers

--- providers.tf
+++ providers.tf
@@ -1,12 +1,27 @@
 terraform {
   required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.49.0"
+    }
+    vault = {
+      source  = "hashicorp/vault"
+      version = "~> 3.12.0"
+    }
+    ldap = {
+      source  = "elastic-infra/ldap"
+      version = "~> 2.0"
+    }
     juju = {
       source  = "juju/juju"
-      version = "0.9.1"
+      version = "~> 0.10.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.9.1"
     }
     random = {
-      source = "hashicorp/random"
-      version = "3.6.0"
+      source  = "hashicorp/random"
+      version = "3.5.1"
     }
-  }
 }
