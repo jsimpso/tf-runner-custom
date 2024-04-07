30b79666ccc3d7776839bfbaffe8730d4728500e
James Simpson <james.simpson@canonical.com>
2024-04-07 16:49:30 +0800

    Bump provider versions

--- tf-runner-juju-ubuntu/providers.tf
+++ tf-runner-juju-ubuntu/providers.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     openstack = {
       source  = "terraform-provider-openstack/openstack"
-      version = "~> 1.49.0"
+      version = "~> 1.54.0"
     }
     vault = {
       source  = "hashicorp/vault"
-      version = "~> 3.12.0"
+      version = "~> 4.2.0"
     }
     ldap = {
       source  = "elastic-infra/ldap"
@@ -19,11 +19,19 @@ terraform {
     }
     time = {
       source  = "hashicorp/time"
-      version = "~> 0.9.1"
+      version = "~> 0.11.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.5.1"
+      version = "~> 3.6.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.2.0"
+    }
+    external = {
+      source  = "hashicorp/external"
+      version = "~> 2.3.3"
     }
     tls = {
       source  = "hashicorp/tls"
